0201cd703ec2704df954b7f9f6a2c2ecdf933f22
kysyl <kysylyov.artem@gmail.com>
2024-09-03 20:07:18 +0300

    working with slider

--- src/components/category-slider/CategorySlider.jsx
+++ src/components/category-slider/CategorySlider.jsx
@@ -2,13 +2,14 @@
 // Imports 
 import React from 'react'
 import styles from './styles.module.scss'
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { sliderData } from '@/sliderData'
 
 // Import Swiper styles
 import 'swiper/scss'
 import 'swiper/scss/navigation'
+import 'swiper/scss/pagination';
 
 // Import components 
 import { CategorySlide } from '@/components/category-slide/CategorySlide'
@@ -17,16 +18,50 @@ export const CategorySlider = () => {
   return (
     <div className={styles.slider}>
       <Swiper
-        modules={[Navigation]}
-            spaceBetween={30}
-            slidesPerView={3.5}
-            loop={true}
-            navigation
-            style={{
-              "--swiper-navigation-color": "#fff",
-          }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+        modules={[Navigation, Pagination]}
+        spaceBetween={30}
+        slidesPerView={3.5}
+        breakpoints={{
+          400: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+            navigation: false,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+            navigation: false,
+          },
+          1000: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            pagination: false,
+            navigation: true,
+          },
+        }}
+        loop={true}
+        pagination={{
+          el: '.swiper-pagination',
+          clickable: true,
+        }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        style={{
+          "--swiper-navigation-color": "#fff",
+        }}
+        
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
       >
         {
           sliderData.map((slide) => (
@@ -41,6 +76,9 @@ export const CategorySlider = () => {
             </SwiperSlide>          
           ))
         }
+        <div className="swiper-pagination"></div>
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
       </Swiper>
     </div>
   )
