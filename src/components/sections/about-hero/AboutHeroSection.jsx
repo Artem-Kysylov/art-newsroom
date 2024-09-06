2db873d101f7ece6de70393cdcde32f6bf93c258
kysyl <kysylyov.artem@gmail.com>
2024-09-06 13:24:08 +0300

    experementing with Image component

--- src/components/sections/about-hero/AboutHeroSection.jsx
+++ src/components/sections/about-hero/AboutHeroSection.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import aboutHero1 from '/public/about-hero-1.png'
+import aboutHero2 from '/public/about-hero-2.png'
+import aboutHero3 from '/public/about-hero-3.png'
 
 // Import components 
 import { Headline } from '@/components/headline/Headline'
@@ -19,26 +22,23 @@ export const AboutHeroSection = () => {
 
             <div className={styles.images__wrapper}>
               <Image
-                src="/about-hero-1.png"
-                width={320}
-                height={376}
-                loading="lazy"
+                src={aboutHero1}
                 alt="hero-image-1"
+                placeholder='blur'
+                style={{maxWidth: '320px', maxHeight: '376px', objectFit: 'cover'}}
               />
               <Image
-                className={styles.image__2}
-                src="/about-hero-2.png"
-                width={494}
-                height={376}
-                loading="lazy"
+              className={styles.image__2}
+                src={aboutHero2}
                 alt="hero-image-2"
+                placeholder='blur'
+                style={{maxWidth: '494px', maxHeight: '376px', objectFit: 'cover'}}
               />
               <Image
-                src="/about-hero-3.png"
-                width={223}
-                height={524}
-                loading="lazy"
+                src={aboutHero3}
                 alt="hero-image-3"
+                placeholder='blur'
+                style={{maxWidth: '223px', maxHeight: '524px', objectFit: 'cover'}}
               />
             </div>
           </div>
