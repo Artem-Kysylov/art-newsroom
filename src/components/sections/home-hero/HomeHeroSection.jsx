d1508d8c584e25f897c8b14ab36584e1cd4b9ede
kysyl <kysylyov.artem@gmail.com>
2024-09-06 19:30:12 +0300

    updates images on home page

--- src/components/sections/home-hero/HomeHeroSection.jsx
+++ src/components/sections/home-hero/HomeHeroSection.jsx
@@ -2,6 +2,11 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Image from 'next/image'
+import homeHero1 from '/public/home-hero-1.png'
+import homeHero2 from '/public/home-hero-2.png'
+import homeHero3 from '/public/home-hero-3.png'
+import homeHero4 from '/public/home-hero-4.png'
+import homeHero5 from '/public/home-hero-5.png'
 
 // Import components 
 import { Headline } from '@/components/headline/Headline'
@@ -13,18 +18,16 @@ export const HomeHeroSection = () => {
             <div className={styles.hero__cta}>
                 <div className={styles.images__wrapper1}>
                     <Image
-                        src="/home-hero-1.png"
-                        width={400}
-                        height={381}
-                        loading="lazy"
+                        src={homeHero1}
                         alt='home-hero-1'
+                        placeholder='blur'
+                        style={{maxWidth: '400px', maxHeight: '381px', objectFit: 'cover'}}
                     />
                     <Image
-                        src="/home-hero-2.png"
-                        width={574}
-                        height={311}
-                        loading="lazy"
+                        src={homeHero2}
                         alt='home-hero-2'
+                        placeholder='blur'
+                        style={{maxWidth: '574px', maxHeight: '311px', objectFit: 'cover'}}
                     />
                 </div>
                 <Headline
@@ -34,36 +37,38 @@ export const HomeHeroSection = () => {
                 />
                 <div className={styles.images__wrapper2}>
                     <Image
-                        src="/home-hero-3.png"
+                        src={homeHero3}
                         width={216}
                         height={280}
-                        loading="lazy"
                         alt='home-hero-3'
+                        placeholder='blur'
+                        style={{maxWidth: '216px', maxHeight: '280px', objectFit: 'cover'}}
                     />
                     <div className={styles.image__2}>
                         <Image                            
-                            src="/home-hero-4.png"
+                            src={homeHero4}
                             width={501}
                             height={313}
-                            loading="lazy"
                             alt='home-hero-4'
+                            placeholder='blur'
+                            style={{maxWidth: '501px', maxHeight: '313px', objectFit: 'cover'}}
                         />
                         <Image 
                             className={styles.text__circle}                           
                             src="/text circle.svg"
                             width={200}
                             height={200}
-                            loading="lazy"
                             alt='text circle'
                         />
                     </div>
                     <Image
                         className={styles.image__3}
-                        src="/home-hero-5.png"
+                        src={homeHero5}
                         width={295}
                         height={414}
-                        loading="lazy"
                         alt='home-hero-5'
+                        placeholder='blur'
+                        style={{maxWidth: '295px', maxHeight: '414px', objectFit: 'cover'}}
                     />
                 </div>
             </div>
