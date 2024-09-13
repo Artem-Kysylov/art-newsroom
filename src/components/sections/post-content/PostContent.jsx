5c7d6e1f5628f66ba34d5f129cf0380c4ce0f895
kysyl <kysylyov.artem@gmail.com>
2024-09-13 19:55:19 +0300

    finish markup of post page

--- src/components/sections/post-content/PostContent.jsx
+++ src/components/sections/post-content/PostContent.jsx
@@ -22,10 +22,55 @@ export const PostContent = () => {
                         borderTopRightRadius: '20px', 
                     }}
                 />
-            <Button
-                text='Share this article'
-                type='outlined'
-            />
+
+                <div className={styles.postContent__wrapper}>
+                    <h3>Title will be here</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <ul className={styles.postContent__list}>
+                        <li>Lorem ipsum dolor sit amet consectetur. </li>
+                        <li>Lorem ipsum dolor sit amet consectetur. </li>
+                        <li>Lorem ipsum dolor sit amet consectetur. </li>
+                        <li>Lorem ipsum dolor sit amet consectetur. </li>
+                    </ul>
+                </div>
+
+                <Image
+                    src='/article-content-img-2.png'
+                    alt='image'
+                    width={800}
+                    height={500}
+                    layout='responsive'
+                    style={{
+                        objectFit: 'cover', 
+                        borderTopLeftRadius: '20px', 
+                        borderTopRightRadius: '20px', 
+                    }}
+                />
+
+                <div className={styles.postContent__wrapper}>
+                    <h3>Title will be here</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <div className={styles.postContent__quoteBlock}>
+                        <p>“Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit.”</p>
+                        <h4>Author name will be here</h4>
+                    </div>
+                </div>
+
+                <div className={styles.postContent__wrapperBottom}>
+                    <h3>Title will be here</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur. Praesent sed nec leo nulla tortor in. Ullamcorper risus scelerisque facilisis eget gravida fusce sodales lectus. Imperdiet ut sit elit ultrices facilisis tellus posuere. Adipiscing in suspendisse ipsum pellentesque elementum aliquet sit. Nam ut semper enim quam sodales fringilla in diam. Elit nec a blandit donec platea. Rhoncus eget quam volutpat sed id nisl.</p>
+                    <div className={styles.postContent__divider}></div>
+
+                    <Button
+                        text='Share this article'
+                        type='outlined'
+                    />
+                </div>
         </div>
     </section>
   )
