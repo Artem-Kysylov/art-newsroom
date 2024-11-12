7cf54f8c4d58d36823a62bbba0fffaf6003eeda8
kysyl <kysylyov.artem@gmail.com>
2024-11-12 19:48:31 +0200

    fetch posts, create more test posts

--- src/app/(client)/page.js
+++ src/app/(client)/page.js
@@ -1,5 +1,6 @@
 // Imports 
 import React from 'react'
+import { client } from '@/sanity/lib/client'
 
 // Import components 
 import { NewsletterForm } from '@/components/newsletter-form/NewsletterForm'
@@ -7,7 +8,26 @@ import { HomeHeroSection } from '@/components/sections/home-hero/HomeHeroSection
 import { NewsroomCategory } from '@/components/newsroom-category/NewsroomCategory'
 import { PopularArticles } from '@/components/sections/popular-articles/PopularArticles'
 
-export default function Home() {
+
+// Fetch all our posts 
+const getPosts = async () => {
+  const query = `
+  * [_type == 'post'] {
+        title,
+        slug,
+        publishedAt,
+        author,
+        category,
+      }
+  `
+  const data = await client.fetch(query)
+  return data
+}
+
+export default async function Home() {
+  const posts = await getPosts()
+  console.log(posts, 'posts')
+
   return (
   <main>
     <div className='sections__wrapper'>
