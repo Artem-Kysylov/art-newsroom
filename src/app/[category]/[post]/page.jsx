c5ffac4e1bc53f8f9faf847b9dc4509afee4c278
kysyl <kysylyov.artem@gmail.com>
2024-11-22 19:04:35 +0200

    add the link

--- src/app/[category]/[post]/page.jsx
+++ src/app/[category]/[post]/page.jsx
@@ -1,14 +1,16 @@
 // Imports 
 import React from 'react'
 
+// Repo link
+// https://github.com/stefandjikic/next-cms-blog
+
 // Import components
 import { PostHeroSection } from '@/components/sections/post-hero/PostHeroSection' 
 import { ArticleContent } from '@/components/sections/article-content/ArticleContent'
 import { InterestingArticles } from '@/components/sections/interesting-articles/InterestingArticles'
 import { NewsletterForm } from '@/components/newsletter-form/NewsletterForm'
 
-const PostPage = () => {
-  
+const PostPage = () => {  
   return (
     <main>
       <div className='sections__wrapper'>
